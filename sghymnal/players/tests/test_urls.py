6d75df6bb0a18f37dec464fc349f8d67f82c4bed
Collin <ococncol@gmail.com>
2020-05-19 19:57:56 -0400

    added test for players_urls

---
+++ sghymnal/players/tests/test_urls.py
@@ -0,0 +1,40 @@
+import pytest
+from django.urls import resolve, reverse
+
+from sghymnal.players.models import Player
+
+pytestmark = pytest.mark.django_db
+
+
+def test_list():
+    assert reverse("players:list") == "/players/"
+    assert resolve("/players/").view_name == "players:list"
+
+
+def test_create():
+    assert reverse("players:create") == "/players/create/"
+    assert resolve("/players/create").view_name == "players:create"
+
+
+def test_detail(player: Player):
+    assert (
+        reverse("players:detail", kwargs={"uuid": player.uuid})
+        == f"/players/{player.uuid}/"
+    )
+    assert resolve(f"/players/{player.uuid}/").view_name == "players:detail"
+
+
+def test_update(player: Player):
+    assert (
+        reverse("player:update", kwargs={"uuid": player.uuid})
+        == f"/players/{player.uuid}/edit/"
+    )
+    assert resolve(f"/players/{player.uuid}/edit/").view_name == "players:update"
+
+
+def test_delete(player: Player):
+    assert (
+        reverse("player:delete", kwargs={"uuid": player.uuid})
+        == f"/players/{player.uuid}/delete/"
+    )
+    assert resolve(f"/players/{player.uuid}/delete/").view_name == "players:delete"
