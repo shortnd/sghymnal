0bb59a7562cb3a22db5929de6910a352ceb2ad23
Collin OConnell <ococncol@gmail.com>
2020-06-07 18:35:57 -0400

    combined api router

--- config/urls.py
+++ config/urls.py
@@ -25,8 +25,6 @@ urlpatterns = [
 urlpatterns += [
     # API base url
     path("api/", include("config.api_router")),
-    # path("api/", include("sghymnal.players.api_router")),
-    # path("api/", include("sghymnal.rosters.api_router")),
     # DRF auth token
     path("auth-token/", obtain_auth_token),
 ]
