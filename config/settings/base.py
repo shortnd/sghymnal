bb1372f348466b28ebb712a4faa8a60da45e6c47
Collin OConnell <ococncol@gmail.com>
2020-06-09 15:48:44 -0400

    pagination??

--- config/settings/base.py
+++ config/settings/base.py
@@ -316,6 +316,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
 }
 # Your stuff...
 # ------------------------------------------------------------------------------
